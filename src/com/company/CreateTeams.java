f07241ce59d8b21fb1875fb81a543f9079e4205a
jhoover4 <jordan@hoovermld.com>
2018-04-18 08:45:05 -0500

    Added read from csv functionality.

---
+++ src/com/company/CreateTeams.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class CreateTeams {
+    final private players[];
+    final private Array expPlayers;
+
+    public CreateTeams(players[]){
+        this.players = players;
+    }
+
+    private void seperatePlayers(){
+        (for int i=0; i<players.length; )
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        String filePath = "league_builder.csv";
+
+        try {
+            Reader in = new FileReader(filePath);
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader("Name", "Height (inches)", "Soccer Experience",
+                    "Guardian Name(s)").parse(in);
+            for (CSVRecord record : records) {
+                String name = record.get("Name");
+                String height = record.get("Height (inches)");
+                String exp = record.get("Soccer Experience");
+                String guardian = record.get("Guardian Name(s)");
+            }
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            filePath + "'");
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
