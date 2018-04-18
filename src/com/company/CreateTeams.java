108de2173189edd7cad34c0bbffc30248d1d4167
Jordan Hoover <jordan@hoovermld.com>
2018-04-18 15:13:53 -0500

    Created team letter writing functionality.

--- src/com/company/CreateTeams.java
+++ src/com/company/CreateTeams.java
@@ -1,16 +1,63 @@
 package com.company;
 
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
-public class CreateTeams {
-    final private players[];
-    final private Array expPlayers;
+class CreateTeams {
+    private List<Map<String, String>> players;
+    private String lettersDirPath;
 
-    public CreateTeams(players[]){
+    CreateTeams(List<Map<String, String>> players) {
         this.players = players;
     }
 
-    private void seperatePlayers(){
-        (for int i=0; i<players.length; )
+    private void createLetterDir(){
+        String newDirStr = System.getProperty("user.dir") + File.separator + "player_letters";
+        Path newDirPath = Paths.get(newDirStr);
+
+        try {
+            Files.createDirectory(newDirPath);
+        }
+        catch (IOException ioe){
+            System.out.println("Directory already created.");
+        }
+
+        this.lettersDirPath = newDirStr;
+    }
+
+    private String createLetterPath(String name){
+        String [] namesArr = name.split("\\s+");
+        String fileName = String.join("_", namesArr).toLowerCase();
+
+        return this.lettersDirPath + File.separator + fileName + ".txt";
+    }
+
+    void createLetters() {
+        createLetterDir();
+
+        for (Map<String, String> player :
+                players) {
+            String heading = String.format("Dear %s,%n%nCongratulations, %s will be playing on the %s this season! ",
+                    player.get("guardian"), player.get("name"), "Sharks");
+            String body = String.format("Please be there for the first practice this Sunday at 1:00pm. " +
+                    "Looking forward to a good season.%n%nRegards,%nCoach Jordan");
+            String fileName = createLetterPath(player.get("name"));
+
+            try {
+                Path path = Paths.get(fileName);
+                String text = heading + body;
+                byte[] strToBytes = text.getBytes();
+                Files.write(path, strToBytes);
+            } catch (IOException ioe) {
+                System.out.println("File not written properly.");
+                ioe.printStackTrace();
+                System.exit(-1);
+            }
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.io.Reader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -12,16 +16,21 @@ public class Main {
 
     public static void main(String[] args) {
         String filePath = "league_builder.csv";
+        List<Map<String, String>> soccerPlayers = new ArrayList<>();
 
         try {
             Reader in = new FileReader(filePath);
             Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader("Name", "Height (inches)", "Soccer Experience",
                     "Guardian Name(s)").parse(in);
             for (CSVRecord record : records) {
-                String name = record.get("Name");
-                String height = record.get("Height (inches)");
-                String exp = record.get("Soccer Experience");
-                String guardian = record.get("Guardian Name(s)");
+                Map<String, String> soccerPlayer = new HashMap<>();
+
+                soccerPlayer.put("name", record.get("Name"));
+                soccerPlayer.put("height", record.get("Height (inches)"));
+                soccerPlayer.put("exp", record.get("Soccer Experience"));
+                soccerPlayer.put("guardian", record.get("Guardian Name(s)"));
+
+                soccerPlayers.add(soccerPlayer);
             }
         }
         catch(FileNotFoundException ex) {
@@ -32,5 +41,8 @@ public class Main {
         catch(IOException ex) {
             ex.printStackTrace();
         }
+        soccerPlayers.remove(0);
+        CreateTeams teamList = new CreateTeams(soccerPlayers);
+        teamList.createLetters();
     }
 }
